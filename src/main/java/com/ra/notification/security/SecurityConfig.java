12105bb87f3dab453e8a75362ac3a41727ae744d
Riswan Agam <riswanagam@gmail.com>
2022-11-12 12:47:36 +0700

    #update security

---
+++ src/main/java/com/ra/notification/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.ra.notification.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        http
+        .cors().and()
+        .csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/api/notification/**").hasRole("ADMIN")
+
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        
+        ;
+        return http.build();
+    }
+}
