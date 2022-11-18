9d8d63e4f122a78c298b13a9ada7af915c38764a
Riswan Agam <riswanagam@gmail.com>
2022-11-18 11:42:31 +0700

    #remove eureka client

--- src/main/java/com/ra/notification/NotificationServerApplication.java
+++ src/main/java/com/ra/notification/NotificationServerApplication.java
@@ -2,12 +2,11 @@ package com.ra.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class NotificationServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServerApplication.class, args);
 	}
